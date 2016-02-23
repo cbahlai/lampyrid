baac0b4dfffee8964a13ce643a5689bbb84881a9
Christie Bahlai <Christie Bahlai>
2016-02-23 14:13:42 -0500

    commented out date  extraction

--- lampyrid_analysis.R
+++ lampyrid_analysis.R
@@ -1,6 +1,15 @@
-# lampyrid
-lampyrid<-read.csv(file="https://ndownloader.figshare.com/files/3686040", header=T)
+# lampyrid analysis
+#bring data in from figshare
+lampyrid<-read.csv(file="https://ndownloader.figshare.com/files/3686040",
+                   header=T)
+
+#clean data
+#fix dates, make them ISO'ed
 library(lubridate)
 lampyrid$newdate<-mdy(lampyrid$DATE)
+#extract year
 lampyrid$year<-year(lampyrid$newdate)
+#extract day of year. DOY is very useful for a phenology type analyses
+#because you don't have to deal with day-of-month numbers starting over 
+#in the middle of a phenological event.
 lampyrid$DOY<-yday(lampyrid$newdate)
