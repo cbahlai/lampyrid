153eca246809f5e2432ca745cbbf7b834ff63b3b
Christie Bahlai <Christie Bahlai>
2016-02-23 16:11:03 -0500

    create broken dd code

--- lampyrid_analysis.R
+++ lampyrid_analysis.R
@@ -16,4 +16,29 @@ lampyrid$DOY<-yday(lampyrid$newdate)
 
 #download weather data from KBS weather station
 weather<-read.table(file="http://lter.kbs.msu.edu/datatables/7.csv",
-                    header=T, sep=",")
+                    header=T, sep=",", na.strings="")
+weather$DOY<-yday(weather$date)
+plot(weather$DOY, weather$air_temp_mean)
+plot(weather$DOY, weather$precipitation)
+
+# calculate the degree day accumulation for the first half of the day dd1,
+#assuming a sine wave structure of temperature over the day
+#use a development threshold of 10C, well, because it's a nice number
+#to work with
+
+thresh<-10
+alpha<-(weather$air_temp_max-weather$air_temp_min)/2
+
+weather$theta<-weather$flag_air_temp_max
+for (i in length(weather$date)) {
+  if (weather$air_temp_max[i]=>thresh) {
+    weather$theta[i]<-asin((10-(weather$air_temp_max[i]+weather$air_temp_min[i])/2)/alpha[i])
+  } else{
+    weather$theta[i]=0
+  }
+}
+weather$dd1<-(1/2*pi*((weather$air_temp_max+weather$air_temp_min)/2)-10)*(pi/2-theta)+
+  
+#triangle method
+  
+weather$
